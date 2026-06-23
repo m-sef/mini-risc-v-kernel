c3f74f0038e59fc3627982bc88fd03eb1f508d1a
m-sef <seth.l.moore0506@gmail.com>
2026-06-23 16:04:59 -0400

    Random BS, NOT WORKING

--- src/context_switch.s
+++ src/context_switch.s
@@ -7,8 +7,37 @@ context_switch:
 #          a1 - New stack pointer
 # Returns: Nothing
 #==============================================================================
+    addi sp, sp, -52 # Grow stack by 13 * 4 bytes
+    sw s11, 48(sp)
+    sw s10, 44(sp)
+    sw s9, 40(sp)
+    sw s8, 36(sp)
+    sw s7, 32(sp)
+    sw s6, 28(sp)
+    sw s5, 24(sp)
+    sw s4, 20(sp)
+    sw s3, 16(sp)
+    sw s2, 12(sp)
+    sw s1, 8(sp)
+    sw s0, 4(sp)
+    sw ra, 0(sp)
+
     sw sp, 0(a0)
 
     mv sp, a1
-    
+
+    lw s11, 48(sp)
+    lw s10, 44(sp)
+    lw s9, 40(sp)
+    lw s8, 36(sp)
+    lw s7, 32(sp)
+    lw s6, 28(sp)
+    lw s5, 24(sp)
+    lw s4, 20(sp)
+    lw s3, 16(sp)
+    lw s2, 12(sp)
+    lw s1, 8(sp)
+    lw s0, 4(sp)
+    lw ra, 0(sp)
+
     ret
--- src/kernel.c
+++ src/kernel.c
@@ -1,27 +1,43 @@
+#include <stdint.h>
+
 extern void print_string(const char* ptr);
-extern void context_switch(unsigned int* old_stack_pointer, unsigned int new_stack_pointer);
+extern void context_switch(uint32_t* old_stack_pointer, uint32_t new_stack_pointer);
 
-unsigned int task_a_stack_pointer;
-unsigned int task_b_stack_pointer;
+uint32_t kernel_stack_pointer;
+uint32_t worker_stack_pointer;
 
-#define PROCESS_STACK_SIZE 1024
-
-static void
-task_a()
-{
-    print_string("Process A\n");
-    context_switch(&task_a_stack_pointer, task_b_stack_pointer);
-}
+#define STACK_SIZE 1024
+uint8_t worker_stack[STACK_SIZE] __attribute__((aligned(16)));
 
 static void
-task_b()
+worker_task()
 {
-    print_string("Process B\n");
+    print_string("Vile Machinations!\n");
+    context_switch(&worker_stack_pointer, kernel_stack_pointer);
 }
 
 int main(void)
 {
-    task_a();
+    __asm__(
+        "addi sp, sp, -52\n"
+        "sw x0, 48(sp)\n"
+        "sw x0, 44(sp)\n"
+        "sw x0, 40(sp)\n"
+        "sw x0, 36(sp)\n"
+        "sw x0, 32(sp)\n"
+        "sw x0, 28(sp)\n"
+        "sw x0, 24(sp)\n"
+        "sw x0, 20(sp)\n"
+        "sw x0, 16(sp)\n"
+        "sw x0, 12(sp)\n"
+        "sw x0, 8(sp)\n"
+        "sw x0, 4(sp)\n"
+        "sw ra, 0(s0)\n"
+    );
+
+    print_string("main: switching INTO worker task...\n");
+    context_switch(&kernel_stack_pointer, worker_stack_pointer);
+    print_string("main: ...back from task. done\n");
 
     return 0;
 }
